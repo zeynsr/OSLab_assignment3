d804204fbbc8fc3a4c597e0cf620cd2f434afb5f
Zeynab Sarafraz <z.srfz.yz@gmail.com>
2023-07-13 21:25:31 +0330

    Update and rename

--- non-repeating_random_numbers.py
+++ non-repeating_random_numbers.py
@@ -1,13 +1,13 @@
 import random
 numbers = []
-counter=int(input('enter the size of array :'))
-i=0
+counter = int(input('enter the size of array :'))
+i = 0
 
-while i<counter:
+while i < counter:
     num = random.randint(1, 1000)
     if not (num in numbers):
         numbers.append(num)
-        i +=1
+        i += 1
     else:
         print("repeated")
 
