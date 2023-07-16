581c9e6f1b871621bfb25fcd6658248fed23f101
Zeynab Sarafraz <z.srfz.yz@gmail.com>
2023-07-17 01:10:20 +0330

    Update and rename

---
+++ LCM.py
@@ -0,0 +1,14 @@
+num1 = int(input())
+num2 = int(input())
+
+if num1 > num2:
+    Min = num2
+else:
+    Min = num1    
+for i in range(1, Min+1):
+    if((num1 % i == 0) and (num2 % i == 0)):
+        gcd = i    
+
+lcm = int((num1 * num2) / gcd)
+
+print('LCM: ', lcm)        
